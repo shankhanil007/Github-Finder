72c0278bd341fd2ddcafd03df66b524c9e8ad361
shankhanil007 <shankhanilborthakur@gmail.com>
2021-05-26 11:41:17 +0530

    Project Setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
+import User from "./components/users/User";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import NotFound from "./components/pages/NotFound";
 
 import "./App.css";
 
@@ -9,6 +13,15 @@ function App() {
     <Router>
       <div className="App">
         <Navbar />
+        <div className="container">
+          <Alert />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/user/:login" component={User} />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
       </div>
     </Router>
   );
