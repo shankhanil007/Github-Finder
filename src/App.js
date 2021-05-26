6f156351d7bdca3f4b583d95e9d95119e966b507
shankhanil007 <shankhanilborthakur@gmail.com>
2021-05-26 11:46:53 +0530

    Fixes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
-import User from "./components/users/User";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import NotFound from "./components/pages/NotFound";
@@ -14,11 +13,9 @@ function App() {
       <div className="App">
         <Navbar />
         <div className="container">
-          <Alert />
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/about" component={About} />
-            <Route exact path="/user/:login" component={User} />
             <Route component={NotFound} />
           </Switch>
         </div>
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,49 +1,30 @@
-import React, { useState, useContext } from 'react';
-import GithubContext from '../../context/github/githubContext';
-import AlertContext from '../../context/alert/alertContext';
+import React, { useState } from "react";
 
 const Search = () => {
-  const githubContext = useContext(GithubContext);
-  const alertContext = useContext(AlertContext);
+  const [text, setText] = useState("");
 
-  const [text, setText] = useState('');
-
-  const onSubmit = e => {
+  const onSubmit = (e) => {
     e.preventDefault();
-    if (text === '') {
-      alertContext.setAlert('Please enter something', 'light');
-    } else {
-      githubContext.searchUsers(text);
-      setText('');
-    }
   };
 
-  const onChange = e => setText(e.target.value);
+  const onChange = (e) => setText(e.target.value);
 
   return (
     <div>
-      <form onSubmit={onSubmit} className='form'>
+      <form onSubmit={onSubmit} className="form">
         <input
-          type='text'
-          name='text'
-          placeholder='Search Users...'
+          type="text"
+          name="text"
+          placeholder="Search Users..."
           value={text}
           onChange={onChange}
         />
         <input
-          type='submit'
-          value='Search'
-          className='btn btn-dark btn-block'
+          type="submit"
+          value="Search"
+          className="btn btn-dark btn-block"
         />
       </form>
-      {githubContext.users.length > 0 && (
-        <button
-          className='btn btn-light btn-block'
-          onClick={githubContext.clearUsers}
-        >
-          Clear
-        </button>
-      )}
     </div>
   );
 };
