11dde8ce74551b2d38285eb79bf934fcd4cfcef9
shankhanil007 <shankhanilborthakur@gmail.com>
2021-05-26 12:47:30 +0530

    Search functionality

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,21 +1,25 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import axios from "axios";
 import Search from "../users/Search";
-// import Users from '../users/Users';
+import Users from "../users/Users";
 
 const Home = () => {
   const githubClientId = process.env.GITHUB_CLIENT_ID;
   const githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
 
+  const [users, setUsers] = useState([]);
+
   const searchUsers = async (text) => {
     const res = await axios.get(
       `https://api.github.com/search/users?q=${text}&client_id=${githubClientId}&client_secret=${githubClientSecret}`
     );
+    setUsers(res.data.items);
   };
 
   return (
     <Fragment>
       <Search searchUsers={searchUsers} />
+      <Users users={users} />
     </Fragment>
   );
 };
