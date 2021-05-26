dde489efa26c86373e63ee485f4cabaacba54826
shankhanil007 <shankhanilborthakur@gmail.com>
2021-05-26 11:39:38 +0530

    Project Setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/layout/Navbar";
 
 import "./App.css";
 
